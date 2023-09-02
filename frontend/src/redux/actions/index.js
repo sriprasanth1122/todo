a344d20030e922b65b3ef4504080438656da8598
sriprasanth1122 <sriprasanth1122@gmail.com>
2023-09-02 13:35:42 +0530

    commit

--- frontend/src/redux/actions/index.js
+++ frontend/src/redux/actions/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { ADDNEW_TODO , GETALL_TODO , TOGGLE_TODO , UPDATE_TODO , DELETE_TODO , TOGGLE_TAB } from './type';
 
 
-const API_URL = 'https://todo-app-4dfh.onrender.com';
+const API_URL = 'https://todo-application-rkbb.onrender.com';
 
 export const addNewTodo = (data) => async (dispatch) => {
     try {
